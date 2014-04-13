58f15f02da7107923dea463fff86400886e8f234
Nicholas Riley <com-github@sabi.net>
2014-04-13 08:13:29 -0500

    Clean up more explicitly on connection ping failure.
    We were getting stuck and not recovering from this error. Unfortunately, I was not able to reproduce the exact situation that caused this failure, but I am relatively confident this will help.

--- dictation.py
+++ dictation.py
@@ -16,16 +16,20 @@ class _Service(object):
         try:
             self.connection.ping(timeout=1)
         except:
+            self.close()
             raise Exception("Can't connect to dictation server (blocked?)")
         self.nesting_level += 1
 
         return self.connection.root
 
+    def close(self):
+        self.connection.close()
+        self.connection = None
+
     def __exit__(self, *exc_info):
         self.nesting_level -= 1
         if self.nesting_level == 0:
-            self.connection.close()
-            self.connection = None
+            self.close()
 
 service = _Service()
 
