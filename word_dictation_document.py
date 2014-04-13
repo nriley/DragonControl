594d3a068082f46b666447fb7a25e8eed9231367
Nicholas Riley <com-github@sabi.net>
2014-04-13 13:09:26 -0500

    Make word_dictation_document.py either get or set the dictation buffer contents.
    Previously, when run from the command line, it simply got the dictation buffer contents, which didn’t entirely fit its name.
    
    Also add an explicit clear_contents function.

--- word_dictation_document.py
+++ word_dictation_document.py
@@ -3,6 +3,8 @@
 import dictation
 import sys
 
+__all__ = ['get_contents', 'set_contents', 'clear_contents']
+
 def get_contents():
     with dictation.service as s:
         return s.get_word_document_contents().strip()
@@ -11,8 +13,32 @@ def set_contents(contents):
     with dictation.service as s:
         s.set_word_document_contents(contents.strip())
 
+def clear_contents():
+    set_contents('')
+
+def get():
+    sys.stdout.write(get_contents().encode('utf-8'))
+
+def set():
+    set_contents(sys.stdin.read())
+
+def usage():
+    print >> sys.stderr, 'usage:', sys.argv[0], '[get|set]'
+    sys.exit(1)
+
 if __name__ == '__main__':
     try:
-        sys.stdout.write(get_contents().encode('utf-8'))
+        if len(sys.argv) == 1:
+            get()
+        elif len(sys.argv) == 2:
+            command = sys.argv[1]
+            if command == 'get':
+                get()
+            elif command == 'set':
+                set()
+            else:
+                usage()
+        else:
+            usage()
     except Exception as e:
         print >> sys.stderr, e
