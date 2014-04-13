9eb846e6036424ca6ebfd0ed7dad76dfdbd0cf2e
Nicholas Riley <com-github@sabi.net>
2014-04-12 22:17:13 -0500

    Use “ignoring application responses” for service menu item selection.
    This works around a bug in Mavericks whereby System Events claims to be unable to click a menu item, but actually succeeds in doing so regardless. We just suppress the error message through the ignoring block in AppleScript, at the expense of reporting a true error.
    
    I should really report this bug to Apple, not that I expect anything will be done about it given my recent history.

--- Receive Dictation (Mail).sh
+++ Receive Dictation (Mail).sh
@@ -1,3 +1,7 @@
 #!/bin/zsh
 
-osascript -e 'tell application "System Events" to click menu item "Receive Dictation" of menu of menu item "Services" of menu "Mail" of menu bar 1 of process "Mail"'
+osascript -e 'tell application "System Events"' \
+		  -e 'ignoring application responses' \
+		  -e 'click menu item "Receive Dictation" of menu of menu item "Services" of menu "Mail" of menu bar 1 of process "Mail"' \
+		  -e 'end ignoring' \
+		  -e 'end tell'
