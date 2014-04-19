2d86068f1f1b815184ab49dffff6efd788b66a4b
Nicholas Riley <com-github@sabi.net>
2014-04-19 09:49:30 -0500

    Growl notifications are redundant at this point. Get rid of them.

--- toggle_mic_state.py
+++ toggle_mic_state.py
@@ -10,15 +10,9 @@ if __name__ == '__main__':
 		if initial_mic_state != 'on':
 			s.set_mic_state('on')
 			s.activate_word()
-			message = 'Dragon is listening.'
 			subprocess.call(['/usr/local/bin/appswitch', '-hi', 'com.vmware.fusion'])
 			subprocess.call(['/usr/local/bin/appswitch', '-si', 'com.vmware.fusion'])
 		else:
 			s.set_mic_state('off')
 			message = 'Dragon is not listening.'
 
-		subprocess.call(['/usr/local/bin/growlnotify',
-			'-t', 'Dragon Medical',
-			'-m', message,
-			'-d', 'mic state',
-			'-I', '/Volumes/Shirley/Users/nicholas/Applications/Dragon Dictate.app'])
