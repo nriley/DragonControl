531d15de83c15859de59194ed1cddf27919a1648
Nicholas Riley <com-github@sabi.net>
2015-06-14 12:04:58 -0400

    Restart directly into an RDP session.
    If the dictation server isn't running when you attempt to restart (or stop) it, don't worry.
    
    Notify that we are restarting/stopping the virtual machine.
    
    While restarting, wait for RDP such that the "Start Dictation via RDP" script does not attempt to start the virtual machine while it is being restarted.

--- restart_server.py
+++ restart_server.py
@@ -1,8 +1,15 @@
 #!/Users/nicholas/Documents/Development/DragonControl/bin/python
 
-import dictation, vm
+import dictation, socket, vm
 
 if __name__ == '__main__':
-	with dictation.service as s:
-		s.stop_server()
+	try:
+		with dictation.service as s:
+			s.stop_server()
+	except socket.error:
+		pass # server isn't running
+	vm.notify('Restarting virtual machine')
 	vm.vmrun('reset')
+	vm.wait_for_rdp()
+	# this is duplicative, but should be idempotent
+	vm.osascript('Start Dictation via RDP')
--- stop_server.py
+++ stop_server.py
@@ -1,8 +1,13 @@
 #!/Users/nicholas/Documents/Development/DragonControl/bin/python
 
-import dictation, vm
+import dictation, socket, sys, vm
 
 if __name__ == '__main__':
-	with dictation.service as s:
-		s.stop_server()
-	vm.vmrun('stop')
+	try:
+		with dictation.service as s:
+			s.stop_server()
+	except socket.error:
+		pass # server isn't running
+	if len(sys.argv) == 1:
+		vm.notify('Shutting down virtual machine')
+		vm.vmrun('stop')
