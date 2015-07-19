fbc77a367b3c60afe9889a6cb58175ee29eddcf1
Nicholas Riley <com-github@sabi.net>
2015-07-19 18:06:51 -0400

    Generalize the Mail workaround as it applies to any application which embeds WebKit.
    Particularly, this includes rich text editing in MarsEdit.
    
    I’m not entirely sure why this is a shell script versus an AppleScript, but whatever…

--- Receive Dictation (WebKit).sh
+++ Receive Dictation (WebKit).sh
@@ -2,6 +2,6 @@
 
 osascript -e 'tell application "System Events"' \
 		  -e 'ignoring application responses' \
-		  -e 'click menu item "Receive Dictation" of menu of menu item "Services" of menu "Mail" of menu bar 1 of process "Mail"' \
+		  -e 'click menu item "Receive Dictation" of menu of menu item "Services" of menu 2 of menu bar 1 of (first process whose frontmost is true)' \
 		  -e 'end ignoring' \
 		  -e 'end tell'
