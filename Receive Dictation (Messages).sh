c5187d2332e4930a343878ed65558dd69c28fb54
Nicholas Riley <com-github@sabi.net>
2015-07-19 18:15:56 -0400

    We can use the WebKit workaround for Messages as well, so remove it as a separate entity.

--- Receive Dictation (Messages).sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-osascript -e 'tell application "System Events"' \
-		  -e 'ignoring application responses' \
-		  -e 'click menu item "Receive Dictation" of menu of menu item "Services" of menu "Messages" of menu bar 1 of process "Messages"' \
-		  -e 'end ignoring' \
-		  -e 'end tell'
