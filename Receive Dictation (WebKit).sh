f2c86a94ea50751e031ee0a82ef9efad93946530
Nicholas Riley <com-github@sabi.net>
2016-08-28 13:10:29 -0400

    Fix shell script styling.

--- Receive Dictation (WebKit).sh
+++ Receive Dictation (WebKit).sh
@@ -1,7 +1,8 @@
-#!/bin/zsh
+#!/bin/zsh -f
 
-osascript -e 'tell application "System Events"' \
-		  -e 'ignoring application responses' \
-		  -e 'click menu item "Receive Dictation" of menu of menu item "Services" of menu 2 of menu bar 1 of (first process whose frontmost is true)' \
-		  -e 'end ignoring' \
-		  -e 'end tell'
+/usr/bin/osascript \
+	-e 'tell application "System Events"' \
+	-e 'ignoring application responses' \
+	-e 'click menu item "Receive Dictation" of menu of menu item "Services" of menu 2 of menu bar 1 of (first process whose frontmost is true)' \
+	-e 'end ignoring' \
+	-e 'end tell'
