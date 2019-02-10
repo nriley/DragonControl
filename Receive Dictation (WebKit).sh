6a2461a5cef8fcd01f4b51820d0ffcf704212252
Nicholas Riley <com-github@sabi.net>
2019-02-10 12:38:11 -0500

    Don't need a shell script that does the same thing as the AppleScript.

--- Receive Dictation (WebKit).sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh -f
-
-/usr/bin/osascript \
-	-e 'tell application "System Events"' \
-	-e 'ignoring application responses' \
-	-e 'click menu item "Receive Dictation" of menu of menu item "Services" of menu 2 of menu bar 1 of (first process whose frontmost is true)' \
-	-e 'end ignoring' \
-	-e 'end tell'
